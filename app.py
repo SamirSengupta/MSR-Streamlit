99b384bcddf6ab79d70ab9f6dad3a8667d0ac301
Samir Sengupta <51231316+SamirSengupta@users.noreply.github.com>
2023-03-13 17:01:48 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -49,7 +49,11 @@ def recommend(movie):
 def find_closest_match(movie_name, movie_titles):
     movie_scores = [(title, fuzz.ratio(movie_name.lower(), title.lower())) for title in movie_titles]
     closest_match = max(movie_scores, key=lambda x: x[1])
-    return closest_match[0]
+    if closest_match[1] >= 70:
+        return closest_match[0]
+    else:
+        return None
+
 
 
 movies_dict = pickle.load(open('movie_dict.pkl','rb'))
@@ -66,36 +70,37 @@ st.write('Enter the name of a movie below to get a list of similar movies:')
 selected_movie_name = st.text_input('Movie Name', value='', key=1, autocomplete=movie_titles_str)
 
 if selected_movie_name:
-    closest_match = find_closest_match(selected_movie_name, movies['title'])
-    movie_matches = movies[movies['title'] == closest_match]
-    
-    if len(movie_matches) == 0:
-        st.write('Sorry, the movie you are searching for is either incorrect or does not exist or may not be present in our database.')
-    else:
-        selected_movie_index = movie_matches.index[0]
-        selected_movie_poster = fetch_poster(movies.iloc[selected_movie_index].movie_id)
-        selected_movie_overview = fetch_movie_details(movies.iloc[selected_movie_index].movie_id)['overview']
-        selected_movie_rating = round(fetch_movie_details(movies.iloc[selected_movie_index].movie_id)['vote_average'], 1)
-        st.write('Selected Movie:', closest_match)
-        st.image(selected_movie_poster, width=200)
-        st.write('Rating:', selected_movie_rating)
-        st.write('Overview:', selected_movie_overview)
-        
+    try:
+        closest_match = find_closest_match(selected_movie_name, movies['title'])
+        movie_matches = movies[movies['title'] == closest_match]
 
-        recommended_movies, recommended_movies_posters, recommended_movies_overview, recommended_movies_ratings = recommend(closest_match)
+        if len(movie_matches) == 0:
+            st.write('Sorry, the movie you are searching for is either incorrect or does not exist or may not be present in our database.')
+        else:
+            selected_movie_index = movie_matches.index[0]
+            selected_movie_poster = fetch_poster(movies.iloc[selected_movie_index].movie_id)
+            selected_movie_overview = fetch_movie_details(movies.iloc[selected_movie_index].movie_id)['overview']
+            selected_movie_rating = round(fetch_movie_details(movies.iloc[selected_movie_index].movie_id)['vote_average'], 1)
+            st.write('Selected Movie:', closest_match)
+            st.image(selected_movie_poster, width=200)
+            st.write('Rating:', selected_movie_rating)
+            st.write('Overview:', selected_movie_overview)
 
-    if recommended_movies:
-        st.write('Recommended Movies:')
-        col1, col2, col3, col4, col5, col6 = st.columns(6)
-        cols = [col1, col2, col3, col4, col5, col6]
-        for i in range(len(recommended_movies)):
-            with cols[i]:
-                st.write(recommended_movies[i])
-                st.image(recommended_movies_posters[i], width=150)
-                st.write('Rating:', recommended_movies_ratings[i])
-                st.write('Overview:', recommended_movies_overview[i])
 
-                
-    else:
-        st.write('Sorry, we could not find any recommendations for this movie.')
+            recommended_movies, recommended_movies_posters, recommended_movies_overview, recommended_movies_ratings = recommend(closest_match)
 
+            if recommended_movies:
+                st.write('Recommended Movies:')
+                col1, col2, col3, col4, col5, col6 = st.columns(6)
+                cols = [col1, col2, col3, col4, col5, col6]
+                for i in range(len(recommended_movies)):
+                    with cols[i]:
+                        st.write(recommended_movies[i])
+                        st.image(recommended_movies_posters[i], width=150)
+                        st.write('Rating:', recommended_movies_ratings[i])
+                        st.write('Overview:', recommended_movies_overview[i])
+
+            else:
+                st.write('Sorry, we could not find any recommendations for this movie.')
+    except:
+        st.write('Sorry, please search for any other movie.')
